e6f83bc6b28ff6f8d6326ce2c60c51a6a4f08c0d
Hong Ly <lyhong.rupp@gmail.com>
2025-12-10 20:27:56 +1100

    install react-icons and logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { FiShoppingBag } from "react-icons/fi";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <FiShoppingBag className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
